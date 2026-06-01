7565525bd6edde992fc4b9bdcc2fba363643d6ff
serhii-sudo <sergeyrudenko1992@gmail.com>
2026-06-01 19:59:30 +0300

    feat: added validations for other form fields

--- store/orders/forms.py
+++ store/orders/forms.py
@@ -5,61 +5,48 @@ from orders.models import Order
 class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
-        exclude = ('created', 'history', 'initiator')
-
+        exclude = ("created", "history", "initiator")
 
     # username (readonly)
     username = forms.CharField(
-        required=False,
-        widget=forms.TextInput(attrs={
-            'class': 'form-control mb-2',
-            'readonly': 'readonly'
-        })
+        required=False, widget=forms.TextInput(attrs={"class": "form-control mb-2", "readonly": "readonly"})
     )
 
     # email
     email = forms.EmailField(
-        required=True,
-        max_length=150,
-        min_length=5,
-        widget=forms.EmailInput(attrs={
-            'class': 'form-control mb-2'
-        })
+        required=True, max_length=150, min_length=5, widget=forms.EmailInput(attrs={"class": "form-control mb-2"})
     )
 
     # first_name
-    first_name = forms.CharField(
-        required=True,
-        widget=forms.TextInput(attrs={
-            'class': 'form-control mb-2'
-        })
-    )
+    first_name = forms.CharField(required=True, widget=forms.TextInput(attrs={"class": "form-control mb-2"}))
 
     # last_name
-    last_name = forms.CharField(
-        required=True,
-        widget=forms.TextInput(attrs={
-            'class': 'form-control mb-2'
-        })
-    )
+    last_name = forms.CharField(required=True, widget=forms.TextInput(attrs={"class": "form-control mb-2"}))
 
     # address
-    address = forms.CharField(
-        required=True,
-        widget=forms.TextInput(attrs={
-            'class': 'form-control mb-2'
-        })
-    )
+    address = forms.CharField(required=True, widget=forms.TextInput(attrs={"class": "form-control mb-2"}))
 
     # mobile
     mobile = forms.CharField(required=True)
 
     # валидируем поле mobile, и записываем его в бд, с префиксом +380
-
     def clean_mobile(self):
-        mobile = ''.join(self.cleaned_data['mobile'].split())
+        mobile = "".join(self.cleaned_data["mobile"].split())
 
         if not mobile.isdigit() or len(mobile) != 9:
-            raise forms.ValidationError('Номер должен содержать 9 цифр')
+            raise forms.ValidationError("Номер должен содержать 9 цифр")
+        return "+380" + mobile
 
-        return '+380' + mobile
+    # валидируем поле email
+    def clean_email(self):
+        email = self.cleaned_data.get("email").strip().lower()
+        return email
+
+    # валидируем несколько полей сразу
+    def clean(self):
+        cleaned_data = super().clean()
+
+        for field_name in ["first_name", "last_name"]:
+            if field_name in cleaned_data:
+                cleaned_data[field_name] = cleaned_data[field_name].strip().lower().capitalize()
+        return cleaned_data
