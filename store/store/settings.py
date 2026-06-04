d014d730cef401d7c8ab52ee6cba237f4d620f0e
serhii-sudo <sergeyrudenko1992@gmail.com>
2026-06-04 20:21:03 +0300

    feat: configure Google OAuth pipeline with username generation compatible with manual registration

--- store/store/settings.py
+++ store/store/settings.py
@@ -144,6 +144,23 @@ AUTHENTICATION_BACKENDS = ("social_core.backends.google.GoogleOAuth2", "django.c
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = os.getenv("SOCIAL_AUTH_GOOGLE_OAUTH2_KEY")
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = os.getenv("SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET")
 
+"GOOGLE PIPELINE"
+SOCIAL_AUTH_PIPELINE = (
+    "social_core.pipeline.social_auth.social_details",
+    "social_core.pipeline.social_auth.social_uid",
+    "social_core.pipeline.social_auth.auth_allowed",
+    "social_core.pipeline.social_auth.social_user",
+    "social_core.pipeline.social_auth.associate_by_email",  # связывает Google аккаунт c username
+    "social_core.pipeline.user.get_username",
+    "social_core.pipeline.user.create_user",
+    "social_core.pipeline.social_auth.associate_user",
+    "social_core.pipeline.social_auth.load_extra_data",
+    "social_core.pipeline.user.user_details",
+)
+
+# разрешает автоматическую ассоциацию social account по email
+SOCIAL_AUTH_ASSOCIATE_BY_EMAIL = True
+
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
 
