8f1098ef5d5d9db3f20a000db39d132c4f3db4b0
serhii-sudo <sergeyrudenko1992@gmail.com>
2026-08-09 22:33:44 +0300

    feat: added Gemini chat history model

--- store/ai_gemini/models.py
+++ store/ai_gemini/models.py
@@ -1,3 +1,35 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+from store import settings
+
+
+# Модель в которую будем сохранять историю чата
+class GeminiMessage(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="gemini_messages",
+    )
+    conversation_id = models.UUIDField(default=uuid.uuid4)
+    role = models.CharField(
+        max_length=20,
+        choices=[
+            ("user", "User"),
+            ("gemini", "Gemini"),
+        ],
+    )
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+        indexes = [
+            models.Index(
+                fields=["user", "conversation_id"],
+            ),
+        ]
+
+    def __str__(self):
+        return f"{self.user.username} | {self.role} | {self.content[:50]}"
